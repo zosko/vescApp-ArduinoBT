53c55a82da124393753ca69dd1d7294617293f9b
bosko <bosko.petreski@gmail.com>
2019-11-13 16:20:52 +0100

    code restructure / clean

--- VESCApp/ViewController.h
+++ VESCApp/ViewController.h
@@ -8,22 +8,12 @@
 
 #import <UIKit/UIKit.h>
 
-typedef struct {
-  float v_in;
-  float current_in;
-  float rpm;
-  float watt_hours;
-  float tachometer_abs;
-} mc_values_packet;
-
 @import CoreBluetooth;
 
 @interface ViewController : UIViewController <CBCentralManagerDelegate, CBPeripheralDelegate,UICollectionViewDelegate,UICollectionViewDataSource>{
     CBCentralManager *centralManager;
     CBPeripheral *connectedPeripheral;
     NSMutableArray *peripherals;
-    CBCharacteristic *txCharacteristic;
-    CBCharacteristicWriteType writeType;
     
     NSArray *arrPedalessData;
     IBOutlet UICollectionView *colPedalessData;
--- VESCApp/ViewController.m
+++ VESCApp/ViewController.m
@@ -9,6 +9,14 @@
 #import "ViewController.h"
 #import "DataCell.h"
 
+typedef struct {
+    float v_in;
+    float current_in;
+    float rpm;
+    float watt_hours;
+    float tachometer_abs;
+} packet;
+
 @interface ViewController ()
 
 @end
@@ -35,7 +43,6 @@
 }
 -(void)centralManager:(CBCentralManager *)central didConnectPeripheral:(CBPeripheral *)peripheral {
     connectedPeripheral = peripheral;
-    txCharacteristic = nil;
     
     [connectedPeripheral setDelegate:self];
     [connectedPeripheral discoverServices:nil];
@@ -56,18 +63,15 @@
         return;
     }
 }
--(void)peripheralIsReadyToSendWriteWithoutResponse:(CBPeripheral *)peripheral{
-    //NSLog(@"peripheralIsReadyToSendWriteWithoutResponse");
-}
 -(void)peripheral:(CBPeripheral *)peripheral didUpdateValueForCharacteristic:(CBCharacteristic *)characteristic error:(NSError *)error {
     if (error) {
         NSLog(@"Error receiving notification for characteristic %@: %@", characteristic, error);
         return;
     }
     
-    mc_values_packet newJoin;
-    [characteristic.value getBytes:&newJoin length:sizeof(newJoin)];
-    [self presentData:newJoin];
+    packet vesc_packet;
+    [characteristic.value getBytes:&vesc_packet length:sizeof(vesc_packet)];
+    [self presentData:vesc_packet];
 }
 -(void)peripheral:(CBPeripheral *)peripheral didDiscoverServices:(NSError *)error {
     for (CBService *service in peripheral.services) {
@@ -80,20 +84,10 @@
         return;
     }
 }
-- (void)peripheral:(CBPeripheral *)peripheral didDiscoverCharacteristicsForService:(CBService *)service error:(NSError *)error {
-    
-    // http://www.hangar42.nl/hm10
-    // BAUD HM-10    115200   //Flashed here http://www.hangar42.nl/ccloader
-    // The HM10 has one service, 0xFFE0, which has one characteristic, 0xFFE1 (these UUIDs can be changed with AT commands by the way)
-    
+-(void)peripheral:(CBPeripheral *)peripheral didDiscoverCharacteristicsForService:(CBService *)service error:(NSError *)error {
     for (CBCharacteristic *characteristic in service.characteristics) {
-        
         if ([characteristic.UUID isEqual:[CBUUID UUIDWithString:@"FFE1"]]) {
-            
-            txCharacteristic = characteristic;
             [peripheral setNotifyValue:YES forCharacteristic:characteristic];
-            
-            writeType = characteristic.properties == CBCharacteristicPropertyWrite ? CBCharacteristicWriteWithResponse : CBCharacteristicWriteWithoutResponse;
         }
     }
 }
@@ -104,11 +98,10 @@
         UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"Are you sure ?" message:@"" preferredStyle:UIAlertControllerStyleAlert];
         
         [alert addAction:[UIAlertAction actionWithTitle:@"Dissconnect" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
-                        
+            
             [self->centralManager cancelPeripheralConnection:self->connectedPeripheral];
             self->connectedPeripheral = nil;
             [self->peripherals removeAllObjects];
-            [sender setTitle:@"READ PEDALESS" forState:UIControlStateNormal];
         }]];
         [alert addAction:[UIAlertAction actionWithTitle:@"Cancel" style:UIAlertActionStyleCancel handler:^(UIAlertAction * _Nonnull action) {
             
@@ -116,8 +109,6 @@
         [self presentViewController:alert animated:YES completion:nil];
     }
     else{
-        
-        [sender setTitle:@"DISCONNECT" forState:UIControlStateNormal];
         [peripherals removeAllObjects];
         [centralManager scanForPeripheralsWithServices:@[[CBUUID UUIDWithString:@"FFE0"]] options:nil];
         [self performSelector:@selector(stopSearchReader) withObject:nil afterDelay:2];
@@ -125,7 +116,7 @@
 }
 
 #pragma mark - CustomFunctions
--(void)presentData:(mc_values_packet)dataVesc {
+-(void)presentData:(packet)dataVesc {
     double wheelDiameter = 700; //mm diameter
     double motorDiameter = 63; //mm diameter
     double gearRatio = motorDiameter / wheelDiameter;
@@ -133,7 +124,7 @@
     
     double ratioRpmSpeed = (gearRatio * 60 * wheelDiameter * M_PI) / ((motorPoles / 2) * 1000000); // ERPM to Km/h
     double ratioPulseDistance = (gearRatio * wheelDiameter * M_PI) / ((motorPoles * 3) * 1000000); // Pulses to km travelled
-
+    
     double speed = dataVesc.rpm * ratioRpmSpeed;
     double distance = dataVesc.tachometer_abs * ratioPulseDistance;
     double power = dataVesc.current_in * dataVesc.v_in;
@@ -144,23 +135,25 @@
                         @{@"title":@"Distance",@"data":[NSString stringWithFormat:@"%.1f km", distance]},
                         @{@"title":@"Speed",@"data":[NSString stringWithFormat:@"%.1f km/h",speed]},
                         @{@"title":@"Power",@"data":[NSString stringWithFormat:@"%.f W",power]}
-                        ];
+    ];
     [colPedalessData reloadData];
 }
-
 -(void)stopSearchReader{
     [centralManager stopScan];
     
     UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"Search device" message:@"Choose Pedaless device" preferredStyle:UIAlertControllerStyleActionSheet];
     for(CBPeripheral *periperal in peripherals){
-        UIAlertAction *action = [UIAlertAction actionWithTitle:[NSString stringWithFormat:@"%@",periperal.name] style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
+        [alert addAction:[UIAlertAction actionWithTitle:[NSString stringWithFormat:@"%@",periperal.name] style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
             [self->centralManager connectPeripheral:periperal options:nil];
-        }];
-        [alert addAction:action];
+        }]];
     }
-    UIAlertAction *cancel = [UIAlertAction actionWithTitle:@"Cancel" style:UIAlertActionStyleDestructive handler:^(UIAlertAction * _Nonnull action) {
-    }];
-    [alert addAction:cancel];
+    
+    [alert addAction:[UIAlertAction actionWithTitle:@"Re-scan pedaless" style:UIAlertActionStyleDestructive handler:^(UIAlertAction * _Nonnull action) {
+        [self onBtnRead:nil];
+    }]];
+    [alert addAction:[UIAlertAction actionWithTitle:@"Cancel" style:UIAlertActionStyleCancel handler:^(UIAlertAction * _Nonnull action) {
+        
+    }]];
     [self presentViewController:alert animated:YES completion:nil];
 }
 
@@ -177,7 +170,6 @@
     cell.lblData.text = dictData[@"data"];
     cell.lblTitle.text = dictData[@"title"];
     
-    
     cell.layer.borderColor = UIColor.lightGrayColor.CGColor;
     cell.layer.borderWidth = 2;
     
@@ -191,9 +183,7 @@
     centralManager = [CBCentralManager.alloc initWithDelegate:self queue:nil];
     peripherals = NSMutableArray.new;
     
-    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(1 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-        [self onBtnRead:nil];
-    });
+    [self performSelector:@selector(onBtnRead:) withObject:nil afterDelay:1];
 }
 -(void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
